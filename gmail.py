ee34fe1e247846ee362d194db980e0c1a4c99709
blevene <blevene@salesforce.com>
2014-02-06 22:49:08 -0500

    Iteration and testing

--- gmail.py
+++ gmail.py
@@ -1,2 +1,21 @@
 #!/bin/python
 from ConfigParser import SafeConfigParser
+import imaplib
+import email
+import re
+
+parser = SafeConfigParser()
+parser.read('.config.txt')
+
+uname = parser.get('login', 'uname')
+p_word = parser.get('login', 'pw')
+
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
+mail.login(uname, p_word)
+mail.select()
+typ, data = mail.search(None, 'ALL')
+for num in data[0].split():
+	typ, data = mail.fetch(num, '(RFC822)')
+	raw_email = data[0][1]
+	email_message = email.message_from_string(raw_email)
+	print email_message['Subject']
